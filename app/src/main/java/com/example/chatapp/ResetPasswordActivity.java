161a2a5cc35d2b34988d38c270f2a7eac39b67cb
phatcharaphon <phatcharaphon_p@cmu.ac.th>
2020-03-19 13:04:42 +0700

    Sprint#1 Completed

---
+++ app/src/main/java/com/example/chatapp/ResetPasswordActivity.java
@@ -0,0 +1,68 @@
+package com.example.chatapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.rengwuxian.materialedittext.MaterialEditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    Button btn_reset;
+    MaterialEditText send_email;
+
+    FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Reset Password");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        btn_reset = findViewById(R.id.btn_reset);
+        send_email = findViewById(R.id.send_email);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        btn_reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = send_email.getText().toString();
+
+                if(email.equals("")){
+                    Toast.makeText(ResetPasswordActivity.this, "All fields required!", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    firebaseAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(ResetPasswordActivity.this, "Please check your Email", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(ResetPasswordActivity.this, LoginActivity.class));
+                            } else {
+                                String error = task.getException().getMessage();
+                                Toast.makeText(ResetPasswordActivity.this, error, Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+
+            }
+        });
+
+    }
+}
